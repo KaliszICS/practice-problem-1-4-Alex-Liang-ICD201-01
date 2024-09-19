1988747d2c4ee86b4325c9720fe228912ca2aad2
Alex-Liang-ICD201-01 <alex.liang3@student.tdsb.on.ca>
2024-09-19 13:43:19 +0000

    Changes

--- PP1_4.py
+++ PP1_4.py
@@ -3,14 +3,28 @@
 def q1():
   #Write Assignment code here
 
+  words = input("input a word")
+  print("your input was "+ words)
+
 def q2():
   #Write Assignment code here
+
+  words2 = input("what is your first name?")
+  print("your first name is " + words2)
 
 def q3():
   #Write Assignment code here
 
+  words3 = input("what is your first name?")
+  words4 = input("what is your last name?")
+  print("your first name is " + words3 " and your last name is " +words4)
+
 def q4():
   #Write Assignment code here
+
+  words5 = input("enter a student's name")
+  words6 = input("enter another student's name")
+  print("your first student is " + words5 " and your second student is " + words6)
 
 #Do not edit code below this comment
 
