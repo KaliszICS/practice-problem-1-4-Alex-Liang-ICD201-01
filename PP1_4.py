08071d19eebf88805ee7e61eb011074afe2f6c29
Alex-Liang-ICD201-01 <alex.liang3@student.tdsb.on.ca>
2024-09-20 13:10:57 +0000

    commitagain

--- PP1_4.py
+++ PP1_4.py
@@ -3,28 +3,28 @@
 def q1():
   #Write Assignment code here
 
-  word = input("input a word")
-  print("your input was "+ word)
+  word = input("Input a word")
+  print("Your input was "+ word)
 
 def q2():
   #Write Assignment code here
 
-  firstname1 = input("what is your first name?")
-  print("your first name is " + firstname1)
+  firstname1 = input("What is your first name?")
+  print("Your first name is " + firstname1)
 
 def q3():
   #Write Assignment code here
 
-  firstname = input("what is your first name?")
-  lastname = input("what is your last name?")
-  print(f"your first name is {firstname} and your last name is {lastname}")
+  firstname = input("What is your first name?")
+  lastname = input("What is your last name?")
+  print(f"Your first name is {firstname} and your last name is {lastname}")
 
 def q4():
   #Write Assignment code here
 
-  student1 = input("enter a student's name")
-  student2 = input("enter another student's name")
-  print("your first student is " + student1 + " and your second student is " + student2)
+  student1 = input("Enter a student's name")
+  student2 = input("Enter another student's name")
+  print("Your first student is " + student1 + " and your second student is " + student2)
 
 #Do not edit code below this comment
 
